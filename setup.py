bc8bd37b68deb633fcb8fa52096d007eed4341b7
dorkusprime <dorkusprime@users.noreply.github.com>
2017-10-23 11:13:03 -0700

    Fleshing out the setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,12 @@ reqs_file.close()
 
 setup(
     name='keen-csv',
+    description='Builds a multiline CSV string from a Keen IO response'
     version='1.0.0',
+    license='MIT',
+    author='Jevon Wild',
+    author_email='jevon@keen.io',
+    url='https://github.com/dorkusprime/keen-csv.py',
     install_requires=reqs,
     packages=['keen-csv']
 )
